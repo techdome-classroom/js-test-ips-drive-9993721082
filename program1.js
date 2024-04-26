27c279b4b7e27edfa46f1768beea562c87057115
9993721082 <161117520+9993721082@users.noreply.github.com>
2024-04-26 13:56:07 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
+  if (nums.length === 0) {
+    return 1;
+}
 
+// Move all positive integers to the front of the list
+let i = 0;
+while (i < nums.length) {
+    if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+        [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    } else {
+        i++;
+    }
+}
 
-    // Implement the function smallest_missing_positive_integer
-  
-  }
-  
-  module.exports = smallestMissingPositiveInteger;
+// Find the smallest missing positive integer
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+        return i + 1;
+    }
+}
 
